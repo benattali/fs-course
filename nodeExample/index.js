eb77a2a830a6955def914acdc1e55855df62eefd
Ben Attali <ben.attali8@gmail.com>
2021-01-02 20:15:38 -0500

    part 3a examples done

--- nodeExample/index.js
+++ nodeExample/index.js
@@ -21,6 +21,9 @@ let notes = [
     important: true
   }
 ]
+
+app.use(express.json())
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!!!!!!!</h1>')
   })
@@ -44,6 +47,34 @@ app.delete('/api/notes/:id', (request, response) => {
     notes = notes.filter(note => note.id !== id)
 
     response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+}
+  
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({ 
+            error: 'content missing' 
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
 })
 
 const PORT = 3001
